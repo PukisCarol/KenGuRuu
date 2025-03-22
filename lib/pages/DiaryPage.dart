4beabd5a31fd90f9bc509227f2b82fce8a7f1ca7
cliffs1 <rokasbergmenas@gmail.com>
2025-03-22 13:55:04 +0200

    upgraded dienorastis
    dienorastis iskeptas kaip sakant

--- lib/pages/DiaryPage.dart
+++ lib/pages/DiaryPage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:kenguruu/pages/ToDoPageDialogButtons.dart';
 
 class DiaryPage extends StatefulWidget {
   const DiaryPage({super.key, required this.title});
@@ -18,9 +19,36 @@ class _DiaryPageState extends State<DiaryPage> {
   ];
 
   void saveDiary() {
-    setState(() {
-      diaryEntries.add(_controller.text);
-    });
+    if(_controller.text.trim().isEmpty) {
+      showDialog(
+        context: context,
+          builder: (context) {
+            return AlertDialog(
+                backgroundColor: Colors.blue,
+                content: SizedBox(
+                    height: 120,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Nieko neįrašėte'),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              MyButton(text: 'Gerai', onPressed: Navigator.of(context).pop)
+                            ],
+                        ),
+                      ],
+                    ),
+                ),
+            );
+          },
+      );
+    } else {
+      setState(() {
+        diaryEntries.add(_controller.text);
+        _controller.clear();
+      });
+    }
   }
 
   @override
@@ -31,18 +59,41 @@ class _DiaryPageState extends State<DiaryPage> {
         title: Text("Dienoraštis"),
         elevation: 0,
       ),
-        body: TextField(
-          controller: _controller,
-          minLines: null,
-          maxLines: null,
-          expands: true,
-          //maxLength: TextField.noMaxLength,
+        body: Column(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                minLines: null,
+                maxLines: null,
+                expands: true,
+                decoration: InputDecoration(
+                  hintText: 'Rašykite savo mintis čia...',
+                  contentPadding: EdgeInsets.all(26),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            if(diaryEntries.isNotEmpty)
+              Container(
+                color:Colors.white,
+                height: 200,
+                child: ListView.builder(
+                  itemCount: diaryEntries.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(diaryEntries[index]),
+                    );
+                  },
+                ),
+              ),
+          ],
         ),
-      bottomSheet: Container(
-        child: ElevatedButton(
-            onPressed: saveDiary,
-            child: Text('Išsaugoti', textAlign: TextAlign.right)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: saveDiary,
+        child: Icon(Icons.save),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
