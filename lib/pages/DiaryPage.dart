06e1732f09b40ffaeb167171c210de1661d019c5
PukisCarol <101592602+PukisCarol@users.noreply.github.com>
2025-04-15 18:08:15 +0300

    mano

--- lib/pages/DiaryPage.dart
+++ lib/pages/DiaryPage.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:kenguruu/pages/ToDoPageDialogButtons.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DiaryPage extends StatefulWidget {
   const DiaryPage({super.key, required this.title});
